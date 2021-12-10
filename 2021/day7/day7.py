b2b972c50fef38dafc809cedaf2d6fc6c1c8e33c
Laura Goon <goon.laura@gmail.com>
2021-12-10 00:02:16 -0800

    day 7 part 2

--- 2021/day7/day7.py
+++ 2021/day7/day7.py
@@ -34,5 +34,53 @@ def crab_position(crabs):
     return prev
 
 
+# crab_subs = read_file('crabs.txt')
+# print(crab_position(crab_subs)) # 328187
+
+'''
+--- PART TWO ---
+'''
+
+# assumes that crabs is sorted already
+def get_feul_count(crabs, pos):
+    crabs_dict = {}
+    for crab in crabs:
+        if crab in crabs_dict.keys():
+            crabs_dict[crab] += 1
+        else:
+            crabs_dict[crab] = 1
+
+    left_arr = list(filter(lambda num: num < pos, crabs))
+    right_arr = list(filter(lambda num: num > pos, crabs))
+
+    left_keys = list(set(left_arr))
+    right_keys = list(set(right_arr))
+
+    left_partialsum = 0
+    right_partialsum = 0
+
+    for key in left_keys:
+        left_partialsum += int(((abs(key-pos)**2 + abs(key-pos)) / 2) * crabs_dict[key])
+    for key in right_keys:
+        right_partialsum += int(((abs(key-pos)**2 + abs(key-pos)) / 2) * crabs_dict[key])
+
+    return left_partialsum + right_partialsum
+
+def crab_position(crabs):
+    if len(crabs) == 1:
+        return crabs[0]
+
+    crabs = sorted(crabs)
+    prev = float('inf')
+
+    for i in range(max(crabs)):
+        curr_feul = get_feul_count(crabs, i)
+        if curr_feul > prev:
+            return prev
+        else:
+            prev = curr_feul
+
+    return prev
+
 crab_subs = read_file('crabs.txt')
-print(crab_position(crab_subs)) # 328187
+print(crab_position(crab_subs)) # 91257582
