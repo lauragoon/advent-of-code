01bdaea7ce809715ef57a3d36238742d02dd3bd6
Laura Goon <goon.laura@gmail.com>
2021-12-10 22:15:01 -0800

    day 9 part 2

--- 2021/day9/day9.py
+++ 2021/day9/day9.py
@@ -75,6 +75,77 @@ def find_risk_levels(heights):
 
     return lo_risk_sum
 
+# heights = read_file('heightmap.txt')
+# print(find_risk_levels(heights))
+
+'''
+--- PART TWO ---
+'''
+
+# edges/cornesr vs. surrounded
+# go down and right
+def dfs_helper(map, i, j, record, size):
+    size_tracker = size
+    record_new = record
+
+    # 9's are not in basins
+    if map[i][j] == 9:
+        return size_tracker, record_new
+
+    # visited
+    if record[i][j] == 1:
+        return size_tracker, record_new
+
+    record_new[i][j] = 1
+    size_tracker += 1
+
+    if j < len(map[0])-1:
+        dfs_out = dfs_helper(map, i, j+1, record_new, size_tracker)
+        size_tracker = dfs_out[0]
+        record_new = dfs_out[1]
+
+    if i < len(map)-1:
+        dfs_out = dfs_helper(map, i+1, j, record_new, size_tracker)
+        size_tracker = dfs_out[0]
+        record_new = dfs_out[1]
+
+    if j > 0:
+        dfs_out = dfs_helper(map, i, j-1, record_new, size_tracker)
+        size_tracker = dfs_out[0]
+        record_new = dfs_out[1]
+
+    if i > 0:
+        dfs_out = dfs_helper(map, i-1, j, record_new, size_tracker)
+        size_tracker = dfs_out[0]
+        record_new = dfs_out[1]
+
+    return size_tracker, record_new
+
+def find_basins(heights):
+    length = len(heights)
+    width = len(heights[0])
+    basin_sizes = []
+    
+    marker_map = [] # marks basins found via dfs
+    for i in range(length):
+        new_row = []
+        for j in range(width):
+            new_row.append(0)
+        marker_map.append(new_row)
+
+    # dfs
+    for i in range(length):
+        for j in range(width):   
+            # check if already basin or cannot be basin
+            if marker_map[i][j] != 1 and heights[i][j] != 9:
+                output = dfs_helper(heights, i, j, marker_map, 0)
+                basin_sizes.append(output[0])
+                marker_map = output[1]
+
+    # find 3 largest basins
+    basin_sizes = sorted(basin_sizes, reverse=True)
+
+    return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
 
 heights = read_file('heightmap.txt')
-print(find_risk_levels(heights))
+print(find_basins(heights))
