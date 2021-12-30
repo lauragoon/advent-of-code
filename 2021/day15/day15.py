82564329cc984c88cd1761611cd499c0d80c0ceb
Laura Goon <goon.laura@gmail.com>
2021-12-29 17:09:17 -0800

    day 15 part 2

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -96,5 +96,62 @@ def find_path(cavern):
     score = dijkstra(cavern)
     return score[len(cavern)-1][len(cavern[0])-1]
 
+# cavern = read_file('cavern.txt')
+# print(find_path(cavern)) # 621
+
+'''
+--- PART TWO ---
+'''
+
+def init_new_cavern(cavern, wider):
+    len_width = len(cavern[0]) * wider
+    len_height = len(cavern) * wider
+    new_ret = []
+
+    for i in range(len_height):
+        new_row = []
+
+        for j in range(len_width):
+            new_row.append(-1)
+
+        new_ret.append(new_row)
+
+    return new_ret
+
+def larger_cavern(cavern):
+    add_map = [[0, 1, 2, 3, 4], \
+               [1, 2, 3, 4, 5], \
+               [2, 3, 4, 5, 6], \
+               [3, 4, 5, 6, 7], \
+               [4, 5, 6, 7, 8]]
+
+    new_cavern = init_new_cavern(cavern, 5)
+    len_width = len(cavern[0])
+    len_height = len(cavern)
+
+    # row in tile
+    for row in range(len(cavern)):
+        # col in tile
+        for col in range(len(cavern[0])):
+
+            # tile rightward
+            for j in range(5):
+                # tile downward
+                for i in range(5):
+                    add_mult = add_map[i][j]
+
+                    sm = cavern[row][col] + add_mult
+                    if sm > 9:
+                        sm -= 9
+
+                    new_cavern[row + (i*len_height)][col + (j*len_width)] = sm
+
+    return new_cavern
+
+def find_path2(cavern):
+    new_cavern = larger_cavern(cavern)
+    score = dijkstra(new_cavern)
+    return score[len(new_cavern)-1][len(new_cavern[0])-1]
+
 cavern = read_file('cavern.txt')
-print(find_path(cavern))
+print(find_path2(cavern)) # 2904
