1fd977ae2f5bb95df4e35e1ac9f59acda53236dc
Laura Goon <goon.laura@gmail.com>
2023-11-01 19:33:41 -0700

    finish filesystem building outline, does it run idk

--- 2022/day07/day07.js
+++ 2022/day07/day07.js
@@ -18,15 +18,15 @@ const ConstantStrings = {
 function Node(name, type, size=null)
 {
   this.name = name;
-  this.type = type;
-  this.size = size;
+  this.type = type; // file or dir
+  this.size = size; // int for file, null for dir
   this.parent = null;
   this.children = [];
 }
 
-function Tree(name)
+function Tree(name, type, size=null)
 {
-  var node = new Node(name);
+  var node = new Node(name, type, size);
   this.root = node;
 }
 
@@ -38,7 +38,6 @@ function Node.LocateChild(name)
     if (currChildName == name)
       return this.children[idx];
   }
-  console.error("Error in LocateChild(): No child with ".concat(name, " name found."));
   return null;
 }
 
@@ -87,6 +86,8 @@ oneCompilerConfig.on("line", function(line)
                 
               default:
                 fileSystemPointer = fileSystemPointer.LocateChild(processedLine[2]);
+                if (fileSystemPointer == null)
+                  console.error("Error in main/LocateChild(): No child with ".concat(name, " name found."));
                 break;
             }
             
@@ -99,7 +100,17 @@ oneCompilerConfig.on("line", function(line)
         
         if (currCommand == ConstantStrings.ls)
         {
-          break; // TODO
+          let data1 = processedLine[0];
+          let data2 = processedLine[1];
+          
+          // don't add duplicate data shown via $ls
+          if (fileSystemPointer.LocateChild(data2)) == null)
+          {
+            if (data1 == ConstantStrings.dir)
+              fileSystemPointer.children.push(Node(data2, ConstantStrings.dir, null))
+            else
+              fileSystemPointer.children.push(Node(data1, ConstantStrings.file, parseInt(data1)))
+          }
         }
         
         else
@@ -109,10 +120,11 @@ oneCompilerConfig.on("line", function(line)
       }
     }
     
-    
-    // console.log(processedLine);
-    
-    // console.log("Part 1 answer: " + part1Ans); // 
-    // console.log("Part 2 answer: " + part2Ans); // 
-    
 });
+
+
+
+// console.log(processedLine);
+    
+// console.log("Part 1 answer: " + part1Ans); // 
+// console.log("Part 2 answer: " + part2Ans); //
